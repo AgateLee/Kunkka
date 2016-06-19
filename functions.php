3aff7a791f3766b2e85efee0112a9748a9048341
AgateLee <liyi5133@126.com>
2016-06-19 15:46:05 +0800

    chang tab to 4 blanks

--- functions.php
+++ functions.php
@@ -58,52 +58,52 @@ remove_action( 'wp_head', 'adjacent_posts_rel_link' );
  * Remove default wordpress widgets
  */
 if( !mutheme_settings('register_widget') ){
-	add_action( 'widgets_init', 'mutheme_unregister_default_widgets', 1 );
+    add_action( 'widgets_init', 'mutheme_unregister_default_widgets', 1 );
 }
 function mutheme_unregister_default_widgets() {
-	unregister_widget( 'WP_Widget_Pages' );
-	unregister_widget( 'WP_Widget_Calendar' );
-	unregister_widget( 'WP_Widget_Archives' );
-	unregister_widget( 'WP_Widget_Links' );
-	unregister_widget( 'WP_Widget_Meta' );
-	unregister_widget( 'WP_Widget_Search' );
-	unregister_widget( 'WP_Widget_Text' );
-	unregister_widget( 'WP_Widget_Categories' );
-	unregister_widget( 'WP_Widget_Recent_Posts' );
-	unregister_widget( 'WP_Widget_Recent_Comments' );
-	unregister_widget( 'WP_Widget_RSS' );
-	unregister_widget( 'WP_Nav_Menu' );
-	unregister_widget( 'WP_Widget_Tag_Cloud' );
+    unregister_widget( 'WP_Widget_Pages' );
+    unregister_widget( 'WP_Widget_Calendar' );
+    unregister_widget( 'WP_Widget_Archives' );
+    unregister_widget( 'WP_Widget_Links' );
+    unregister_widget( 'WP_Widget_Meta' );
+    unregister_widget( 'WP_Widget_Search' );
+    unregister_widget( 'WP_Widget_Text' );
+    unregister_widget( 'WP_Widget_Categories' );
+    unregister_widget( 'WP_Widget_Recent_Posts' );
+    unregister_widget( 'WP_Widget_Recent_Comments' );
+    unregister_widget( 'WP_Widget_RSS' );
+    unregister_widget( 'WP_Nav_Menu' );
+    unregister_widget( 'WP_Widget_Tag_Cloud' );
 }
 
 /**
  * Post thumbnail custom sizes
  */
 if ( function_exists( 'add_image_size' ) ) {
-	add_image_size( 'index-thumbnail', 250, 250, true );
+    add_image_size( 'index-thumbnail', 250, 250, true );
 }
 
 /**
  * Register wordpress menu
  */
 if ( function_exists( 'register_nav_menus' ) ) {
-	register_nav_menus( array(
-		'top-menu'    => __( 'Top menu', MUTHEME_NAME ),
-		'global-menu' => __( 'Dropdown menu', MUTHEME_NAME )
-	) );
+    register_nav_menus( array(
+        'top-menu'    => __( 'Top menu', MUTHEME_NAME ),
+        'global-menu' => __( 'Dropdown menu', MUTHEME_NAME )
+    ) );
 }
 
 /**
  * Register sidebar
  */
 if ( function_exists( 'register_sidebar' ) ) {
-	register_sidebar( array(
-		'name'          => 'sidebar',
-		'id'            => 'sidebar-page',
-		'before_widget' => '<div></div>',
-		'before_title'  => '<h1>',
-		'after_title'   => '</h1>'
-	) );
+    register_sidebar( array(
+        'name'          => 'sidebar',
+        'id'            => 'sidebar-page',
+        'before_widget' => '<div></div>',
+        'before_title'  => '<h1>',
+        'after_title'   => '</h1>'
+    ) );
 }
 
 /**
