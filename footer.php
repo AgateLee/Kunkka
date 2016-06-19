e23d3b584a7347cfb0f6eccc4faffdc271e55145
AgateLee <liyi5133@126.com>
2016-06-19 15:12:12 +0800

    样式修改
    修改了header、footer、style

--- footer.php
+++ footer.php
@@ -2,10 +2,11 @@
 </div>
 <!-- #content -->
 <div id="footer">
-	<div class="container">
-		<p>&copy; 2016 <a href="/"><?php bloginfo( 'name' ); ?></a> All Rights Reserved. <a href="http://mufeng.me/">Kunkka
-				theme</a> powered by <a href="http://wordpress.org/">WordPress</a>.</p>
-	</div>
+    <div class="container">
+        <p>&copy; 2016 <a href="/"><?php bloginfo( 'name' ); ?></a> All Rights Reserved. <a href="http://mufeng.me/">Kunkka theme</a> powered by <a href="http://wordpress.org/">WordPress</a>.</p> 
+        <p><a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备16001742号</a></p>
+        <p>&nbsp</p>
+    </div>
 </div>
 <!-- #footer -->
 <?php wp_footer(); ?>
