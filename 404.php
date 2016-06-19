45af9365477d91104d2c347e3d607f2f3aa298d7
AgateLee <liyi5133@126.com>
2016-06-19 15:05:18 +0800

    本地化
    替换了图片，增加了表情及表情脚本，替换了404页面，增加了模板

--- 404.php
+++ 404.php
@@ -1,9 +1,41 @@
-<?php get_header(); ?>
-<div id="primary">
-	<div id="postlist">
-		<p><?php _e('The page not found or not this page', MUTHEME_NAME);?></p>
-		<p><?php _e('You can also try to search the post!', MUTHEME_NAME);?></p>
-	</div>
-</div>
-<?php get_sidebar();?>
-<?php get_footer(); ?>	
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ */
+get_header(); ?>
+    <div id="primary" class="full-page">
+        <div class="errorDiv">
+            <a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home">
+                <img src="<?php echo mutheme_image( '404.png' ); ?>" alt="<?php bloginfo( 'name' ); ?>" width="55%"; />
+            </a>
+            <p><h1>Not Found</h1></p>
+            <p>It looks like nothing was found here. Maybe try a search?</p>
+            <p>Or return to homepage after <span id="timedown" style="font-size:2em;color:#EB4444"></span> s </p>
+        </div>
+    </div>
+    <!-- #primary -->
+    <style>
+    .errorDiv{
+        margin: 0 auto;
+        text-align: center;
+    }
+    </style>
+    <script type="text/javascript">  
+    //设定倒数秒数  
+    var t = 24;  
+    //显示倒数秒数  
+    function showTime(){  
+        t -= 1;  
+        document.getElementById('timedown').innerHTML= t;  
+        if(t==0){  
+            location.href='http://agatelee.cn';  
+        }  
+        //每秒执行一次,showTime()  
+        setTimeout("showTime()",1000);  
+    }  
+
+    //执行showTime()  
+    showTime();  
+    </script>  
+<?php get_footer(); ?>
